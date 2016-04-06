d48527d0dc07fb2f749ead7079072800c1a5f215
MikosJon <atletjm@gmail.com>
2016-04-06 21:25:40 +0200

    Update add_ship_alpha.py
    Mogoče me jutr ne bo --> probi popravt

--- add_ship_alpha.py
+++ add_ship_alpha.py
@@ -7,13 +7,13 @@ def make_board(a):
 
 	rows = columns = a
 
-	size.append(rows)
-	size.append(columns)
-
 	if rows < 5:
 		rows = 5
 	if columns < 5:
 		columns = 5
+
+	size.append(rows + 2)
+	size.append(columns + 2)
 
 	board.append(["X"] * (columns))
 	for i in range(rows):
@@ -31,39 +31,35 @@ def add_ship(n, board):
 	rows = size[0]
 	columns = size[1]
 
-	dic = {
-	0: [0, -1],
-	1: [0, 1],
-	2: [-1, 0],
-	3: [1, 0]
-	}
+	dic = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 
-	space = 0
+	
 	exit = False
 
 	while not exit:
-		ship_beg_row = ship_row = randint(0, rows - 1)
-		ship_beg_col = ship_col = randint(0, columns - 1)
-		ship_beg_direction = ship_direction = randint(0,3)
+		space = 0
+		no_space = False
+		bound = False
+
+		ship_row = randint(0, rows - 1)
+		ship_col = randint(0, columns - 1)
+		ship_direction = randint(0,3)
 
 		if board[ship_row][ship_col] == "O":
 			for i in range(1, n):
 				if board[ship_row + i * dic[ship_direction][0]][ship_col + i * dic[ship_direction][1]] == "X":
-					space += i
+					space += (i - 1)
+					bound = True
 					for j in range(n - 1 - space):
 						if board[ship_row - j * dic[ship_direction][0]][ship_col - j * dic[ship_direction][1]] == "X":
-							'''
-							FIX HERE!
-											    ____________________________
-							   _____                          ,\\    ___________________    \
-							  |     `------------------------'  ||  (___________________)   `|
-							  |_____.------------------------._ ||  ____________________     |
-							                                  `//__(____________________)___/
-
-							'''
+							no_space = True
 							break
 
+					if no_space:
+						break
+
 					for k in range(n):
+						# ko je smer 3, začne risat navzdol in se teleportira čez rob
 						board[ship_row + (space - k) * dic[ship_direction][0]] \
 						[ship_col + (space - k) * dic[ship_direction][1]] = "S"
 
@@ -73,6 +69,7 @@ def add_ship(n, board):
 						board[ship_row + (space - k) * dic[ship_direction][0] - dic[ship_direction][1]] \
 						[ship_col + (space - k) * dic[ship_direction][1] - dic[ship_direction][0]] = "X"
 
+					# ne izriše X v kotih ladje	
 					board[ship_row + (space + 1) * dic[ship_direction][0]] \
 					[ship_col + (space + 1) * dic[ship_direction][1]] = "X"
 
@@ -81,21 +78,25 @@ def add_ship(n, board):
 
 					exit = True
 					break
-					
-			for k in range(n):
-				board[ship_row - k * dic[ship_direction][0]] \
-				[ship_col - k * dic[ship_direction][1]] = "S"
 
-				board[ship_row - k * dic[ship_direction][0] + dic[ship_direction][1]] \
-				[ship_col - k * dic[ship_direction][1] + dic[ship_direction][0]] = "X"
+			if not bound:
+				for k in range(n):
+					# tu dobi out of range, če je smer 2 in ne najde X
+					board[ship_row - (k * dic[ship_direction][0])] \
+					[ship_col + (k * dic[ship_direction][1])] = "S"
 
-				board[ship_row - k * dic[ship_direction][0] - dic[ship_direction][1]] \
-				[ship_col - k * dic[ship_direction][1] - dic[ship_direction][0]] = "X"
+					board[ship_row - (k * dic[ship_direction][0]) + dic[ship_direction][1]] \
+					[ship_col + (k * dic[ship_direction][1]) + dic[ship_direction][0]] = "X"
 
-			board[ship_row + dic[ship_direction][0]] \
-			[ship_col + dic[ship_direction][1]] = "X"
+					board[ship_row - (k * dic[ship_direction][0]) - dic[ship_direction][1]] \
+					[ship_col + (k * dic[ship_direction][1]) - dic[ship_direction][0]] = "X"
 
-			board[ship_row - n * dic[ship_direction][0]] \
-			[ship_col - n * dic[ship_direction][1]] = "X"
+				# včasih napiše X na sredino ladje, ker ne gledava dolžine
+				board[ship_row + dic[ship_direction][0]] \
+				[ship_col + dic[ship_direction][1]] = "X"
 
-			exit = True
+				board[ship_row - dic[ship_direction][0]] \
+				[ship_col - dic[ship_direction][1]] = "X"
+
+				# ne izriše X v kotih ladje
+				exit = True
