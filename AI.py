e39055e60b8877ceaca24def1717594a5a190ac3
zmrzlikar jakob <jakobzmrzlikar@gimvic.local>
2016-06-02 16:58:09 +0200

    popravu svoje

--- AI.py
+++ AI.py
@@ -9,92 +9,123 @@ class AI:
         self.wrong_dir = False
         self.compass = None
 
+    def find_coords(self, i):
+        if i == 0:
+            self.guess_coords = [self.hit_coords[0] + 1, self.hit_coords[-1]]
+        elif i == 1:
+            self.guess_coords = [self.hit_coords[0] - 1, self.hit_coords[-1]]
+        elif i == 2:
+            self.guess_coords = [self.hit_coords[0], self.hit_coords[-1] + 1]
+        elif i == 3:
+            self.guess_coords = [self.hit_coords[0], self.hit_coords[-1] - 1]
+
+
+
+    def hit_guess(self):
+        self.hit = self.game.hit
+        self.previous_miss = self.game.previous_miss
+        self.hit_coords = self.game.hit_coords
+
+        #če je prej zadel(je na pravi premici)
+        if self.hit >= 2:
+            #če je v tej smeri še prostor
+            if not self.wrong_dir:
+                if self.dir == 0:
+                    self.guess_coords = [self.guess_coords[0] + 1, self.guess_coords[-1]]
+                elif self.dir == 1:
+                    self.guess_coords = [self.guess_coords[0] - 1, self.guess_coords[-1]]
+                elif self.dir == 2:
+                    self.guess_coords = [self.guess_coords[0], self.guess_coords[-1] + 1]
+                elif self.dir == 3:
+                    self.guess_coords = [self.guess_coords[0], self.guess_coords[-1] - 1]
+            #če v tej smeri ni več prostora (ampak je na pravi premici) gre v nasprotno smer
+            elif self.wrong_dir:
+                #če je prejšnjo zgrešil se nanaša na originalen zadetek
+                if self.previous_miss:
+                    if self.dir == 0:
+                        self.guess_coords = [self.hit_coords[0] - 1, self.hit_coords[-1]]
+                    elif self.dir == 1:
+                        self.guess_coords = [self.hit_coords[0] + 1, self.hit_coords[-1]]
+                    elif self.dir == 2:
+                        self.guess_coords = [self.hit_coords[0], self.hit_coords[-1] - 1]
+                    elif self.dir == 3:
+                        self.guess_coords = [self.hit_coords[0], self.hit_coords[-1] + 1]
+                #če je prejšnjo zadel se nanaša na prejšnjo
+                else:
+                    if self.dir == 0:
+                        self.guess_coords = [self.guess_coords[0] - 1, self.guess_coords[-1]]
+                    elif self.dir == 1:
+                        self.guess_coords = [self.guess_coords[0] + 1, self.guess_coords[-1]]
+                    elif self.dir == 2:
+                        self.guess_coords = [self.guess_coords[0], self.guess_coords[-1] - 1]
+                    elif self.dir == 3:
+                        self.guess_coords = [self.guess_coords[0], self.guess_coords[-1] + 1]
+
+        #če je prvič zadel ali če je prejšnjo zgrešil
+        else:
+            self.guess_board = []
+            for i in range(len(self.game.board_com)):
+                self.guess_board.append([0 for j in range(len(self.game.board_com))])
+            for a in self.game.player_ships:
+                self.statistical_guess(a)
+
+            x = self.hit_coords[0]
+            y = self.hit_coords[-1]
+            target_array = [self.guess_board[x+1][y], self.guess_board[x-1][y], self.guess_board[x][y+1], self.guess_board[x][y-1]]
+            target = max(target_array)
+            for i, j in enumerate(target_array):
+                if i == target:
+                    self.dir = j
+                    find_coords(j, target_array)
+
+
+
 
     def statistical_guess(self, n):
-        self.mode = self.game.style
+        for i in range (1, len(self.game.board_com)-1):
+            for j in range(1, len(self.game.board_com[i])-1):
+                if self.game.board_com[i][j] not in ["G", "D"]:
+                    obstructed = False
+                    for k in range(1, n):
+                        if self.game.board_com[i][j+k] in ["G", "D"] or j+k > (len(self.game.board_com) - 2):
+                            obstructed = True
+                            break
+                    if not obstructed:
+                        for k in range(n):
+                            print(len(self.guess_board), self.hit)
 
-        if self.mode == "hit":
-            self.hit = self.game.hit
-            self.previous_miss = self.game.previous_miss
-            self.hit_coords = self.game.hit_coords
+                            self.guess_board[i][j+k] += 1
 
-            #če je prej zadel(je na pravi premici)
-            if self.hit >= 2:
-                #če je v tej smeri še prostor
-                if not self.wrong_dir:
-                   self.guess_coords = [self.guess_coords[0] + self.compass[0], self.guess_coords[-1] + self.compass[-1]]
-                   #če je naslednji že zaseden
-                   if self.game.board_com[self.guess_coords[0], self.guess_coords[-1]] in ["G", "H", "D"]:
-                        self.wrong_dir = True
-                        self.guess_coords = [self.hit_coords[0] - compass[0], self.hit_coords[-1] - compass[-1]]
-                #če v tej smeri ni več prostora (ampak je na pravi premici) gre v nasprotno smer
-                elif self.wrong_dir:
-                    #če je prejšnjo zgrešil se nanaša na originalen zadetek
-                    if self.previous_miss:
-                        self.guess_coords = [self.hit_coords[0] - compass[0], self.hit_coords[-1] - compass[-1]]
-                    #če je prejšnjo zadel se nanaša na prejšnjo
-                    else:
-                        self.guess_coords = [self.guess_coords[0] - compass[0], self.guess_coords[-1] - compass[-1]]
-            #če prej v tej smeri ni zadel
-            elif self.compass and self.game.board_com[self.hit_coords[0] + self.compass[0]][self.hit_coords[-1] + self.compass[-1]]:
-                self.compass_array.remove(self.compass)
-                print(self.compass_array)
-                if len(self.compass_array) == 1:
-                    self.compass = self.compass_array[0]
-                else:   
-                    self.compass = self.compass_array[randint(0, len(self.compass_array)-1)]
-                self.guess_coords = [self.hit_coords[0] + self.compass[0], self.hit_coords[-1] + self.compass[-1]]
-            #če je prvič zadel
-            else:
-                self.compass_array = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-                for i in self.compass_array:
-                    if self.game.board_com[self.guess_coords[0]+i[0]][self.guess_coords[1]+i[-1]] in ["G", "H", "D"]:
-                        self.compass_array.remove(i)
-                    #here goes the hepler function :)
-                if len(self.compass_array) == 1:
-                    self.compass = self.compass_array[0]
-                else:   
-                    self.compass = self.compass_array[randint(0, len(self.compass_array)-1)]
-                self.guess_coords = [self.guess_coords[0] + self.compass[0], self.guess_coords[-1] + self.compass[-1]]
-
-
-        if self.mode == "statistical":
-            for i in range (1, len(self.game.board_com)-1):
-                for j in range(1, len(self.game.board_com[i])-1):
-                    if self.game.board_com[i][j] not in ["G", "H", "D"]:
-                        obstructed = False
-                        for k in range(1, n):
-                            if self.game.board_com[i][j+k] in ["G", "H", "D"] or j+k > (len(self.game.board_com) - 2):
-                                obstructed = True
-                                break
-                        if not obstructed:
-                            for k in range(n):
-                                self.guess_board[i][j+k] += 1
-
-            for i in range (1, len(self.game.board_com)-1):
-                for j in range(1, len(self.game.board_com[i])-1):
-                    if self.game.board_com[i][j] not in ["G", "H", "D"]:
-                        obstructed_2 = False
-                        for k in range(1, n):
-                            if self.game.board_com[i+k][j] in ["G", "H", "D"] or i+k > (len(self.game.board_com) - 2):
-                                obstructed_2 = True
-                                break
-                        if not obstructed_2:
-                            for k in range(n):
-                                self.guess_board[i+k][j] += 1
+        for i in range (1, len(self.game.board_com)-1):
+            for j in range(1, len(self.game.board_com[i])-1):
+                if self.game.board_com[i][j] not in ["G", "D"]:
+                    obstructed_2 = False
+                    for k in range(1, n):
+                        if self.game.board_com[i+k][j] in ["G", "D"] or i+k > (len(self.game.board_com) - 2):
+                            obstructed_2 = True
+                            break
+                    if not obstructed_2:
+                        for k in range(n):
+                            self.guess_board[i+k][j] += 1
 
     def guess(self, board):
-        self.guess_board = []
         max_list = []
         max_all = 0
         self.pos_guess = []
+        self.mode = self.game.style
 
-        # if self.mode == "random":
-        for i in range(len(self.game.board_com)):
-            self.guess_board.append([0 for j in range(len(self.game.board_com))])
+        
 
-        for k in self.game.player_ships:
-            self.statistical_guess(k)
+        if self.mode == "statistical":
+            self.guess_board = []
+            for i in range(len(self.game.board_com)):
+                self.guess_board.append([0 for j in range(len(self.game.board_com))])
+
+            for k in self.game.player_ships:
+                self.statistical_guess(k)
+
+        elif self.mode == "hit":
+            self.hit_guess()
 
         for row in self.guess_board:
             print(str(row))
@@ -113,8 +144,3 @@ class AI:
         self.guess_coords = self.pos_guess[pos]
         self.game.guess_ship(self.guess_coords[0], self.guess_coords[1], board)
         print(self.guess_coords)
-        
-
-        # else:
-            # pass            
-
